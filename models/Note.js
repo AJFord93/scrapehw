b601d4dd311b14fe7fa4d2328d2ae0df124affa5
AJ Ford <AJCranford1019@gmail.com>
2017-05-17 18:13:46 -0400

    Fin

---
+++ models/Note.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let NoteSchema = new Schema({
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
+});
+
+const Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
---
+++ server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const Note = require("./models/Note.js");
+const Article = require("./models/Article.js");
+
+const request = require("request");
+const cheerio = require("cheerio");
+
+mongoose.Promise = Promise;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/scrapehw");
+const db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+app.get("/scrape", function(req, res) {
+
+  request("http://www.reddit.com/", function(error, response, html) {
+
+    let $ = cheerio.load(html);
+
+    $(".title").each(function(i, element) {
+
+
+      let result = {};
+
+      result.title = $(this).children('a').text();
+      result.link = $(this).children().attr('href');
+
+      let entry = new Article(result);
+      console.log(result);
+
+      entry.save(function(err, doc) {
+
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  res.send("Scrape Complete");
+});
+
+app.get("/articles", function(req, res) {
+
+  Article.find({}, function(error, doc) {
+
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+
+app.get("/articles/:id", function(req, res) {
+
+  Article.findOne({ "_id": req.params.id })
+  .populate("note")
+  .exec(function(error, doc) {
+
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+
+
+app.post("/articles/:id", function(req, res) {
+  let newNote = new Note(req.body);
+
+  newNote.save(function(error, doc) {
+
+    if (error) {
+      console.log(error);
+    } else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      .exec(function(err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
